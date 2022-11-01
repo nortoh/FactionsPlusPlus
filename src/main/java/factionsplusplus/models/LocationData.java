ac731087c95d1eac2e67c7160f58bef96da25328
Matt B <320696+mattwb65@users.noreply.github.com>
2022-10-31 23:14:50 -0400

    switch LocationData to use Double to encompass all cases that would use it

--- src/main/java/factionsplusplus/models/LocationData.java
+++ src/main/java/factionsplusplus/models/LocationData.java
@@ -2,6 +2,7 @@ package factionsplusplus.models;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
@@ -11,53 +12,60 @@ import java.util.UUID;
 
 public class LocationData {
     @ColumnName("x_position")
-    private Integer x = null;
+    private Double x = null;
     @ColumnName("y_position")
-    private Integer y = null;
+    private Double y = null;
     @ColumnName("z_position")
-    private Integer z = null;
+    private Double z = null;
     @ColumnName("world_id")
     private UUID world;
 
     public LocationData() { }
     
     public LocationData(Integer x, Integer y, Integer z, UUID world) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.x = Double.valueOf(x);
+        this.y = Double.valueOf(y);
+        this.z = Double.valueOf(z);
         this.world = world;
     }
 
+    public LocationData(Location location) {
+        this.x = location.getX();
+        this.y = location.getY();
+        this.z = location.getZ();
+        this.world = location.getWorld().getUID();
+    }
+
     public LocationData(Block block) {
-        this.x = block.getX();
-        this.y = block.getY();
-        this.z = block.getZ();
+        this.x = Double.valueOf(block.getX());
+        this.y = Double.valueOf(block.getY());
+        this.z = Double.valueOf(block.getZ());
         this.world = block.getWorld().getUID();
     }
 
     public LocationData(Chunk chunk) {
-        this.x = chunk.getX();
-        this.z = chunk.getZ();
+        this.x = Double.valueOf(chunk.getX());
+        this.z = Double.valueOf(chunk.getZ());
         this.world = chunk.getWorld().getUID();
     }
 
     public LocationData(LocationDataBean bean) {
-        this.x = bean.getX();
-        this.y = bean.getY();
-        this.z = bean.getZ();
+        this.x = Double.valueOf(bean.getX());
+        this.y = Double.valueOf(bean.getY());
+        this.z = Double.valueOf(bean.getZ());
         this.world = bean.getWorld();
     }
 
     public Integer getX() {
-        return this.x;
+        return this.x != null ? this.x.intValue() : null;
     }
     
     public Integer getY() {
-        return this.y;
+        return this.y != null ? this.y.intValue() : null;
     }
 
     public Integer getZ() {
-        return this.z;
+        return this.z != null ? this.z.intValue() : null;
     }
 
     public UUID getWorld() {
@@ -65,10 +73,19 @@ public class LocationData {
     }
 
     public Block getBlock() {
-        return Bukkit.getWorld(this.world).getBlockAt(this.x, this.y, this.z);
+        return Bukkit.getWorld(this.world).getBlockAt(this.x.intValue(), this.y.intValue(), this.z.intValue());
     }
 
     public Chunk getChunk() {
-        return Bukkit.getWorld(this.world).getChunkAt(this.x, this.z);
+        return Bukkit.getWorld(this.world).getChunkAt(this.x.intValue(), this.z.intValue());
+    }
+
+    public Location getLocation() {
+        return new Location(
+            Bukkit.getWorld(this.world),
+            this.x,
+            this.y,
+            this.z
+        );
     }
 }
