9588b8bafba1f0a1e7e152e651a46bdf5d47ed84
Matt B <320696+mattwb65@users.noreply.github.com>
2022-11-10 16:06:00 -0500

    improve help

--- src/main/java/factionsplusplus/commands/HelpCommand.java
+++ src/main/java/factionsplusplus/commands/HelpCommand.java
@@ -9,11 +9,13 @@ import com.google.inject.Singleton;
 
 import factionsplusplus.models.Command;
 import factionsplusplus.models.CommandContext;
+import factionsplusplus.utils.Pagination;
 import factionsplusplus.utils.StringUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ComponentLike;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 import org.bukkit.command.CommandSender;
 
@@ -22,6 +24,7 @@ import factionsplusplus.data.repositories.CommandRepository;
 import factionsplusplus.builders.ArgumentBuilder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Spliterator;
 import java.util.stream.Collectors;
 import java.util.List;
@@ -59,11 +62,11 @@ public class HelpCommand extends Command {
             String requestedCommand = context.getStringArgument("page or command");
             Command command = this.commandRepository.get(requestedCommand);
             if (command == null) {
-                context.error("Error.CommandNotFound");
+                context.error("Error.CommandNotFound", requestedCommand);
                 return;
             }
             List<ComponentLike> components = new ArrayList<>();
-            components.add(Component.translatable("Help.Command.Title").args(Component.text(command.getName())).decorate(TextDecoration.BOLD).color(NamedTextColor.LIGHT_PURPLE));
+            components.add(Component.translatable("Help.Command.Title").args(Component.text(command.getFullName())).decorate(TextDecoration.BOLD).color(NamedTextColor.LIGHT_PURPLE));
             components.add(Component.translatable("Help.Command.Description").color(NamedTextColor.AQUA).args(Component.text(command.getDescription())));
             if (command.getAliases().length > 0) components.add(Component.translatable("Help.Command.Aliases").color(NamedTextColor.AQUA).args(Component.text(String.join(", ", command.getAliases()))));
             if (command.hasSubCommands()) {
@@ -78,21 +81,16 @@ public class HelpCommand extends Command {
             });
             return;
         }
-        final ArrayList<ArrayList<Command>> partitionedList = this.generateHelpPages();
-        requestedPage--; // pages start at 0
-        if (requestedPage > partitionedList.size()-1) {
-            requestedPage = partitionedList.size()-1; // Upper Limit over LAST_PAGE
-        }
-        if (requestedPage < 0) {
-            requestedPage = 0; // Lower Limit to 0
-        }
-        context.replyWith("Help.ListTitle", requestedPage+1, partitionedList.size());
-        context.replyWithMiniMessage(
-            partitionedList.get(requestedPage).stream().map(cmd -> {
-                final String syntax = cmd.buildSyntax().length() > 0 ? String.format(" %s", cmd.buildSyntax()) : "";
-                return String.format("<color:gold>/mf %s%s:</color:gold> %s", cmd.getName(), syntax, cmd.getDescription());
-            }).collect(Collectors.joining("\n"))
+        final Pagination pagination = new Pagination(
+            MiniMessage.miniMessage().deserialize("<color:gray><lang:HelpCommand.Description>"),
+            this.generateCommandListMiniMessages()
         );
+        final List<Component> pageResults = pagination.generatePage(requestedPage);
+        if (pageResults == null) {
+            context.error("Error.Pagination.NoSuchPage", requestedPage);
+            return;
+        }
+        pageResults.forEach(context.getExecutorsAudience()::sendMessage);
     }
 
     public ArrayList<ArrayList<Command>> generateHelpPages() {
@@ -107,12 +105,45 @@ public class HelpCommand extends Command {
         return result;
     }
 
+    public String buildCommandSyntax(Command command) {
+        return command.buildSyntax().length() > 0 ? String.format(" %s", command.buildSyntax()) : "";
+    }
+
+    public List<String> generateCommandHelp(Command command) {
+        List<String> results = new ArrayList<>();
+        if (! command.shouldHideParentCommand()) results.add(String.format("<color:gold>/f %s%s:</color:gold> %s", command.getName(), this.buildCommandSyntax(command), command.getDescription()));
+        if (command.shouldShowSubCommandsSeparately() && command.hasSubCommands()) {
+            command.getSubCommands().values().stream().forEach(cmd -> {
+                results.add(String.format("<color:gold>/f %s %s%s:</color:gold> %s", command.getName(), cmd.getName(), this.buildCommandSyntax(cmd), cmd.getDescription()));
+            });
+        }
+        return results;
+    }
+
+    public List<Component> generateCommandListMiniMessages() {
+        List<String> results = new ArrayList<>();
+        this.commandRepository
+            .all()
+            .values()
+            .stream()
+            .forEach(command -> {
+                results.addAll(this.generateCommandHelp(command));
+            });
+        return Pagination.partsFromMiniMessages(results);
+    }
+
     public List<String> autocompletePage(CommandSender sender, String argument, List<String> rawArguments) {
         List<String> completions = new ArrayList<>();
         List<String> argsToCompare = IntStream.range(1, this.generateHelpPages().size()+1).mapToObj(String::valueOf).collect(Collectors.toList());
         if (argument.length() == 0 || StringUtils.parseAsInteger(argument) == null) {
+            final List<String> spaceSplit = new ArrayList<String>(Arrays.asList(argument.split(" ")));
             if (argument.length() != 0) argsToCompare.clear();
-            argsToCompare.addAll(this.commandRepository.all().keySet());
+            if (spaceSplit.size() > 1 || argument.endsWith(" ")) {
+                final String commandName = argument.endsWith(" ")  ? argument.trim() : spaceSplit.subList(0, spaceSplit.size() - 1).stream().collect(Collectors.joining(" ")).trim();
+                argument = spaceSplit.size() > 1 ? spaceSplit.get(spaceSplit.size() - 1) : "";
+                argsToCompare.addAll(this.commandRepository.get(commandName).getSubCommands().keySet());
+            }
+            else argsToCompare.addAll(this.commandRepository.all().keySet());
         }
         org.bukkit.util.StringUtil.copyPartialMatches(argument, argsToCompare, completions);
         return completions;
